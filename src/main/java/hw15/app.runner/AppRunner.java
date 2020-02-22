32d4cd4f91364fdb88d06df539dbc8f1b0da241c
MarkianPylat23 <marek.pt23@gmail.com>
2020-02-22 23:44:17 +0200

    remake mistakes for hw15

--- src/main/java/hw15/app.runner/AppRunner.java
+++ src/main/java/hw15/app.runner/AppRunner.java
@@ -2,18 +2,12 @@ package hw15.app.runner;
 
 import hw15.operation.WordOperation;
 
-import java.io.IOException;
-
 public class AppRunner {
     private WordOperation fileManager = new WordOperation();
 
     public void runApp() {
-        try {
-            fileManager.countAllWords();
-            fileManager.countShortWords();
-            fileManager.showMostRepeatedWords();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        fileManager.addWords();
+        fileManager.addShortWords();
+        fileManager.addMostRepeatedWords();
     }
 }
--- src/main/java/hw15/operation/WordOperation.java
+++ src/main/java/hw15/operation/WordOperation.java
@@ -6,78 +6,98 @@ import java.util.Map;
 import java.util.Set;
 
 public class WordOperation {
-    private String bufferedReader = "src/main/resources/data.txt";
-    private String bufferedWriter = "src/main/resources/addData.txt";
+    private String bufferedReaderPath = "src/main/resources/data.txt";
+    private String bufferedWriterPath = "src/main/resources/addData.txt";
 
-    public void countAllWords() throws IOException {
-        FileReader frForCountWords = new FileReader(bufferedReader);
-        BufferedReader brForCountWords = new BufferedReader(frForCountWords);
+    public void addWords() {
 
-        String currentLine;
-        int counterOfWords = 0;
-        while ((currentLine = brForCountWords.readLine()) != null) {
-            String[] songInArray = currentLine.split(" ");
-            counterOfWords = counterOfWords + songInArray.length;
+        try {
+            FileReader frForCountWords = new FileReader(bufferedReaderPath);
+            BufferedReader brForCountWords = new BufferedReader(frForCountWords);
+
+            String currentLine;
+            int counterOfWords = 0;
+
+            while ((currentLine = brForCountWords.readLine()) != null) {
+                String[] songInArray = currentLine.split(" ");
+                counterOfWords = counterOfWords + songInArray.length;
+            }
+            frForCountWords.close();
+            System.out.println("Amount of words in song: " + counterOfWords + "\n");
+
+        } catch (IOException wordError) {
+            System.out.println("Any words, try again");
         }
-        frForCountWords.close();
-        System.out.println("Amount of words in song: " + counterOfWords + "\n");
     }
 
-    public void countShortWords() throws IOException {
-        FileReader frForShortWords = new FileReader(bufferedReader);
-        BufferedReader brForShortWords = new BufferedReader(frForShortWords);
-        FileWriter fwForShortWords = new FileWriter(bufferedWriter);
-        BufferedWriter bwForShortWords = new BufferedWriter(fwForShortWords);
+    public void addShortWords() {
 
-        String currentLine;
-        int numberOfShortWords = 0;
-        while ((currentLine = brForShortWords.readLine()) != null) {
-            String[] shortWordsInArray = currentLine.split(" ");
+        try {
+            FileReader frForShortWords = new FileReader(bufferedReaderPath);
+            BufferedReader brForShortWords = new BufferedReader(frForShortWords);
 
-            for (String shortWords : shortWordsInArray) {
-                if (shortWords.length() <= 3 && !shortWords.equals("" + "")) {
-                    bwForShortWords.write(shortWords + "\n");
-                    bwForShortWords.flush();
+            FileWriter fwForShortWords = new FileWriter(bufferedWriterPath);
+            BufferedWriter bwForShortWords = new BufferedWriter(fwForShortWords);
 
-                    numberOfShortWords++;
+            String currentLine;
+            int numberOfShortWords = 0;
+
+            while ((currentLine = brForShortWords.readLine()) != null) {
+                String[] shortWordsInArray = currentLine.split(" ");
+
+                for (String shortWords : shortWordsInArray) {
+                    if (shortWords.length() <= 3 && !shortWords.equals("" + "")) {
+                        bwForShortWords.write(shortWords + "\n");
+                        bwForShortWords.flush();
+
+                        numberOfShortWords++;
+                    }
                 }
+                System.out.println("Amount of short words in song: " + numberOfShortWords + "\n");
+                frForShortWords.close();
+                fwForShortWords.close();
             }
+        } catch (IOException wordError) {
+            System.out.println("Any words, try again");
         }
-        frForShortWords.close();
-        fwForShortWords.close();
-        System.out.println("Amount of short words in song: " + numberOfShortWords + "\n");
     }
 
-    public void showMostRepeatedWords() throws IOException {
-        FileReader frForRepeatedWords = new FileReader(bufferedReader);
-        BufferedReader brForRepeatedWords = new BufferedReader(frForRepeatedWords);
-        HashMap<String, Integer> wordCountMap = new HashMap<String, Integer>();
+    public void addMostRepeatedWords() {
 
-        String currentLine = brForRepeatedWords.readLine();
-        while (currentLine != null) {
-            String[] words = currentLine.toLowerCase().split(" ");
-            for (String repeatedWord : words) {
-                if (wordCountMap.containsKey(repeatedWord)) {
-                    wordCountMap.put(repeatedWord, wordCountMap.get(repeatedWord) + 1);
-                } else {
-                    wordCountMap.put(repeatedWord, 1);
+        try{
+            FileReader frForRepeatedWords = new FileReader(bufferedReaderPath);
+            BufferedReader brForRepeatedWords = new BufferedReader(frForRepeatedWords);
+            HashMap<String, Integer> wordCountMap = new HashMap<>();
+
+            String currentLine = brForRepeatedWords.readLine();
+            while (currentLine != null) {
+                String[] words = currentLine.toLowerCase().split(" ");
+                for (String repeatedWord : words) {
+                    if (wordCountMap.containsKey(repeatedWord)) {
+                        wordCountMap.put(repeatedWord, wordCountMap.get(repeatedWord) + 1);
+                    } else {
+                        wordCountMap.put(repeatedWord, 1);
+                    }
+                }
+                currentLine = brForRepeatedWords.readLine();
+            }
+
+            String mostRepeatedWord = null;
+            int count = 0;
+            Set<Map.Entry<String, Integer>> entrySet = wordCountMap.entrySet();
+
+            for (Map.Entry<String, Integer> entry : entrySet) {
+                if (entry.getValue() > count) {
+                    mostRepeatedWord = entry.getKey();
+                    count = entry.getValue();
                 }
             }
-            currentLine = brForRepeatedWords.readLine();
-        }
+            System.out.println("The most repeated word in input file is : " + mostRepeatedWord + "\n");
+            System.out.println("Amount of repeats : " + count);
+            brForRepeatedWords.close();
 
-        String mostRepeatedWord = null;
-        int count = 0;
-        Set<Map.Entry<String, Integer>> entrySet = wordCountMap.entrySet();
-
-        for (Map.Entry<String, Integer> entry : entrySet) {
-            if (entry.getValue() > count) {
-                mostRepeatedWord = entry.getKey();
-                count = entry.getValue();
-            }
+        }catch (IOException wordError) {
+            System.out.println("Any words, try again");
         }
-        System.out.println("The most repeated word in input file is : " + mostRepeatedWord + "\n");
-        System.out.println("Amount of repeats : " + count);
-        brForRepeatedWords.close();
     }
 }
